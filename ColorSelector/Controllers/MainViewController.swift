4516954305999a256eeba8755f4e3b6f9b00b9da
Wassim Zerouta <wassim.zerouta@gmail.com>
2023-09-08 15:14:43 +0200

    Sort project folders and create UIImage extension to get average color

--- ColorSelector/Controllers/MainViewController.swift
+++ ColorSelector/Controllers/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  ColorSelector
 //
 //  Created by Wass on 08/09/2023.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
